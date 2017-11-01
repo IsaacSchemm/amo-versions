210a8320eb3479ba66fdc64475a0815becbe7362
Isaac Schemm <isaacschemm@gmail.com>
2017-11-01 13:18:24 -0500

    Update HTML app to show add-on name

--- amo-versions/app.ts
+++ amo-versions/app.ts
@@ -1,28 +1,39 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+﻿const viewModel = {
+    addon: ko.observable<any>(),
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
+    name: ko.observable<string>(),
+    summary: ko.observable<string>(),
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
+    versions: ko.observableArray<any>()
 }
 
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+function getString(map: { [key: string]: string | undefined }, fallback_language: string) {
+    return map[navigator.language] || map[fallback_language] || null;
+}
+
+window.onload = async () => {
+    const main = document.getElementById("main");
+
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get('id');
+    const page = searchParams.get('page') || "1";
+
+    if (id == null) {
+        main!.innerHTML = "Use the ?id= parameter to specify an add-on, using a slug, GUID, or numeric ID.";
+        return;
+    }
+
+    ko.applyBindings(viewModel, main);
+
+    const addon = await fetch(`https://addons.mozilla.org/api/v3/addons/addon/${id}`)
+        .then(r => r.json());
+    viewModel.addon(addon);
+    
+    viewModel.name(getString(addon.name, addon.default_locale));
+    viewModel.summary(getString(addon.summary, addon.default_locale));
+
+    const versions = await fetch(`https://addons.mozilla.org/api/v3/addons/addon/${id}/versions?page=${page}`)
+        .then(r => r.json())
+        .then(o => o.results);
+    viewModel.versions(versions);
 };
