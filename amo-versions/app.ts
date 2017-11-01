eace646964d35f1ddfd4b89c75efaf5d6cfafdfb
Isaac Schemm <isaacschemm@gmail.com>
2017-11-01 13:37:50 -0500

    Get version info

--- amo-versions/app.ts
+++ amo-versions/app.ts
@@ -7,8 +7,42 @@
     versions: ko.observableArray<any>()
 }
 
-function getString(map: { [key: string]: string | undefined }, fallback_language: string) {
-    return map[navigator.language] || map[fallback_language] || null;
+function getString(map: { [key: string]: string | undefined } | null, fallback_language?: string) {
+    if (map == null) return null;
+    return map[navigator.language] || (fallback_language && map[fallback_language]) || null;
+}
+
+function getReleasedStr(version: any) {
+    return new Date(version.files[0].created).toLocaleDateString(navigator.language, {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
+}
+
+function getCompatiblityStr(version: any) {
+    const applications = [
+        { id: "firefox", name: "Firefox" },
+        { id: "android", name: "Firefox for Android" },
+        { id: "thunderbird", name: "Thunderbird" },
+        { id: "seamonkey", name: "SeaMonkey" }
+    ];
+    let strs: string[] = [];
+    for (const app of applications) {
+        const c = version.compatibility[app.id];
+        if (c) {
+            if (c.max == "*") {
+                strs.push(`${app.name} ${c.min} and above`);
+            } else {
+                strs.push(`${app.name} ${c.min} - ${c.max}`);
+            }
+        }
+    }
+    return strs.join(", ");
+}
+
+function getReleaseNotes(version: any) {
+    return getString(version.release_notes);
 }
 
 window.onload = async () => {
