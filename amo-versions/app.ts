e4fd72509b8334637a42c4b2b11842fc974a16b1
Isaac Schemm <isaacschemm@gmail.com>
2017-11-03 10:36:18 -0500

    Only show install button if extension is installable

--- amo-versions/app.ts
+++ amo-versions/app.ts
@@ -96,6 +96,10 @@ class FlatVersion {
 
     readonly compatibility_display: KnockoutComputed<string>;
 
+    readonly target: string;
+    readonly app_name: string;
+    readonly app_compatible: KnockoutComputed<boolean>;
+
     constructor(readonly version: AmoVersion) {
         this.file = [
             ...version.files.filter(f => f.platform == platform || f.platform == "all"),
@@ -153,6 +157,87 @@ class FlatVersion {
 
             return compatiblityStrs.join(", ");
         });
+
+        this.target = /SeaMonkey/.test(navigator.userAgent) ? "seamonkey"
+            : /PaleMoon/.test(navigator.userAgent) ? "palemoon"
+                : /Thunderbird/.test(navigator.userAgent) ? "thunderbird"
+                    : /Android/.test(navigator.userAgent) ? "android"
+                        : "firefox";
+        this.app_name = /SeaMonkey/.test(navigator.userAgent) ? "SeaMonkey"
+            : /PaleMoon/.test(navigator.userAgent) ? "Pale Moon"
+                : /Thunderbird/.test(navigator.userAgent) ? "Thunderbird"
+                    : "Firefox";
+        this.app_compatible = ko.pureComputed(() => {
+            const ext_file = this.ext_file();
+            const amo_compat = this.version.compatibility[this.target];
+            switch (this.target) {
+                case "palemoon":
+                    // TODO more logic here
+                    return true;
+                case "seamonkey":
+                case "thunderbird":
+                    if (!amo_compat) return false;
+                    if (!this.checkMinVersion(amo_compat.min)) return false;
+                    if (!ext_file) return true;
+                    if (ext_file.has_webextension) return false;
+                    if (ext_file.is_strict_compatibility_enabled) {
+                        if (!this.checkMaxVersion(amo_compat.max)) return false;
+                    }
+                    return true;
+                default:
+                    if (!amo_compat) return false;
+                    if (!this.checkMinVersion(amo_compat.min)) return false;
+                    if (this.version.is_strict_compatibility_enabled) {
+                        if (!this.checkMaxVersion(amo_compat.max)) return false;
+                    }
+                    return true;
+            }
+        });
+    }
+
+    getAppVersion(): string {
+        const versionMatch = /SeaMonkey\/([0-9\.]+)/.exec(navigator.userAgent)
+            || /PaleMoon\/([0-9\.]+)/.exec(navigator.userAgent)
+            || /rv:([0-9\.]+)/.exec(navigator.userAgent);
+        return versionMatch ? versionMatch[1] : "999";
+    }
+
+    checkMinVersion(min: string) {
+        const addonMinVersion = min.split('.');
+        
+        const myVersion = this.getAppVersion().split('.');
+
+        for(let i = 0; i < addonMinVersion.length; i++) {
+            const theirMin = addonMinVersion[i] == '*'
+                ? 0
+                : +addonMinVersion[i];
+            const mine = myVersion.length > i
+                ? myVersion[i]
+                : 0;
+            if (theirMin > mine) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkMaxVersion(max: string) {
+        const addonMaxVersion = max.split('.');
+
+        const myVersion = this.getAppVersion().split('.');
+
+        for(let i = 0; i < addonMaxVersion.length; i++) {
+            const theirMax = addonMaxVersion[i] == '*'
+                ? Infinity
+                : +addonMaxVersion[i];
+            const mine = myVersion.length > i
+                ? myVersion[i]
+                : 0;
+            if (theirMax < mine) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
